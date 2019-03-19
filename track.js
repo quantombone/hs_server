189e936619d9834c79ab73df1274771265e82830
Tomasz Malisiewicz <tom@vision.ai>
2019-03-19 01:20:58 -0700

    add flip button

--- track.js
+++ track.js
@@ -1,20 +1,25 @@
 const video = document.getElementById("myvideo");
-const handimg = document.getElementById("handimage");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-let trackButton = document.getElementById("trackbutton");
-let nextImageButton = document.getElementById("nextimagebutton");
 let updateNote = document.getElementById("updatenote");
 
-let imgindex = 1
 let isVideo = false;
 let model = null;
 
 // video.width = 500
 // video.height = 400
 
-const modelParams = {
-    flipHorizontal: true,   // flip e.g for video  
+function flip_function() {
+  //var element = document.getElementById("myvideo");
+  //element.classList.toggle("flipped");
+
+
+  modelParams.flipHorizontal = !modelParams.flipHorizontal;
+  model.setModelParameters(modelParams);
+}
+
+var modelParams = {
+    flipHorizontal: false,   // flip e.g for video
     maxNumBoxes: 20,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
     scoreThreshold: 0.6,    // confidence threshold for predictions.
@@ -25,35 +30,26 @@ function startVideo() {
         console.log("video started", status);
 	video.play();
         if (status) {
-            updateNote.innerText = "Video started. Now tracking"
-            isVideo = true
-            runDetection()
+            updateNote.innerText = "Video started. Now tracking";
+            isVideo = true;
+            runDetection();
         } else {
-            updateNote.innerText = "Please enable video"
+            updateNote.innerText = "Please enable video";
         }
     });
 }
 
 function toggleVideo() {
     if (!isVideo) {
-        updateNote.innerText = "Starting video"
+        updateNote.innerText = "Starting video";
         startVideo();
     } else {
-        updateNote.innerText = "Stopping video"
-        handTrack.stopVideo(video)
+        updateNote.innerText = "Stopping video";
+        handTrack.stopVideo(video);
         isVideo = false;
-        updateNote.innerText = "Video stopped"
+        updateNote.innerText = "Video stopped";
     }
 }
-
-function nextImage() {
-
-    imgindex++;
-    handimg.src = "images/" + imgindex % 15 + ".jpg"
-    runDetectionImage(handimg)
-}
-
-
 
 function runDetection() {
     model.detect(video).then(predictions => {
@@ -78,25 +74,20 @@ function runDetectionImage(img) {
 // Load the model.
 handTrack.load(modelParams).then(lmodel => {
     // detect objects in the image.
-    model = lmodel
-    updateNote.innerText = "Loaded Model!"
+    model = lmodel;
+    updateNote.innerText = "Loaded Model!";
 
     handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
-	runDetectionImage(handimg)
         if (status) {
-            updateNote.innerText = "Video started. Now tracking"
-            isVideo = true
-            runDetection()
+            updateNote.innerText = "Video started. Now tracking";
+            isVideo = true;
+            runDetection();
         } else {
-            updateNote.innerText = "Please enable video"
+            updateNote.innerText = "Please enable video";
         }
     });
-    
 
-    //trackButton.disabled = false
-    //nextImageButton.disabled = false
 });
 
 //toggleVideo();
-
